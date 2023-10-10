ec66fefd4ddd52095c76c0d99681dfea20ffc2ba
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-10 18:45:27 +0900

    enum の理解

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -8,26 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myMind: String = "nothing!"
-    @State var isChangedMind: Bool = false
-
-    var body: some View {
-        VStack {
-            Text(myMind)
-            Button {
-                isChangedMind.toggle()
-                myMind = getMind(with: isChangedMind)
-            } label: {
-                Text("Change my mind!")
-            }
-        }
+    @State var menu: Menu = .beef
+    
+    enum Menu {
+        case beef, pork, chicken
     }
-
-    func getMind(with isChangedMind: Bool) -> String {
-        if isChangedMind {
-            return "always open"
-        } else {
-            return "but closed"
+    
+    var body: some View {
+        if menu == .beef {
+            Text("Today is beef")
+        } else if menu == .pork {
+            Text("Today is pork")
+        } else if menu == .chicken {
+            Text("Today is chicken")
+        }
+        else {
+            Text("What is dinner?")
         }
     }
 }
