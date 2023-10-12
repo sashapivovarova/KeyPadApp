18f729215dfc7ce4a37eb90cfddd69faf2a3dae7
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-12 14:28:13 +0900

    section の理解

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -7,40 +7,70 @@
 
 import SwiftUI
 
-struct Person: Identifiable{
-    var id = UUID()
-    let name: String
-    let imageName: String
-}
-
 struct ContentView: View {
     var body: some View {
-        //        List {
-        //            HStack {
-        //                Image(systemName: "heart.fill")
-        //                    .foregroundColor(.red)
-        //                Text("Sasha")
-        //            }
-        //            HStack {
-        //                Image(systemName: "heart")
-        //                    .foregroundColor(.black)
-        //                Text("Jamie")
-        //            }
-        //            HStack {
-        //                Image(systemName: "heart.fill")
-        //                    .foregroundColor(.red)
-        //                Text("Dakota")
-        //            }
-        //        }
-        
-        var people: [Person] = [Person(name: "Sasha", imageName: "heart.fill"),
-                                Person(name: "Jamie", imageName: "heart"),
-                                Person(name: "Dakota", imageName: "heart.fill")]
-        
-        List(people) {person in
-            HStack {
-                Image(systemName: person.imageName)
-                Text(person.name)
+        List {
+            Section {
+                HStack {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                    Text("Sasha")
+                }
+                HStack {
+                    Image(systemName: "heart")
+                        .foregroundColor(.black)
+                    Text("Jamie")
+                }
+                HStack {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                    Text("Dakota")
+                }
+            } header: {
+                Text("a class")
+            }
+            Section {
+                HStack {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                    Text("Sasha")
+                }
+                HStack {
+                    Image(systemName: "heart")
+                        .foregroundColor(.black)
+                    Text("Jamie")
+                }
+                HStack {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                    Text("Dakota")
+                }
+            } header: {
+                Text("b class")
+            }
+            Section {
+                HStack {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                    Text("Sasha")
+                }
+                HStack {
+                    Image(systemName: "heart")
+                        .foregroundColor(.black)
+                    Text("Jamie")
+                }
+                HStack {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                    Text("Dakota")
+                }
+            } header: {
+                Text("c class")
+            } footer: {
+                VStack{
+                    Text("Created by Sasha")
+                    Text("Contact : @sasha")
+                }
             }
         }
     }
