7fd8cab14dc932822ef61b1fef8362c4100f727b
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-12 17:35:58 +0900

    dial model を作成

---
+++ MyFirstApp/Dial.swift
@@ -0,0 +1,14 @@
+//
+//  Dial.swift
+//  MyFirstApp
+//
+//  Created by Anastasia Grey on 2023/10/12.
+//
+
+import Foundation
+
+struct Dial: Identifiable {
+    var id = UUID()
+    var mainNumber: String
+    var subAlphabet: [String]
+}
