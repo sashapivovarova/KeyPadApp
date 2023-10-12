e760c01eda7203e165f9b0900ee0d181a14ac2c8
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-12 14:14:17 +0900

    dynamic list の理解

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -7,23 +7,40 @@
 
 import SwiftUI
 
+struct Person: Identifiable{
+    var id = UUID()
+    let name: String
+    let imageName: String
+}
+
 struct ContentView: View {
     var body: some View {
-        List {
+        //        List {
+        //            HStack {
+        //                Image(systemName: "heart.fill")
+        //                    .foregroundColor(.red)
+        //                Text("Sasha")
+        //            }
+        //            HStack {
+        //                Image(systemName: "heart")
+        //                    .foregroundColor(.black)
+        //                Text("Jamie")
+        //            }
+        //            HStack {
+        //                Image(systemName: "heart.fill")
+        //                    .foregroundColor(.red)
+        //                Text("Dakota")
+        //            }
+        //        }
+        
+        var people: [Person] = [Person(name: "Sasha", imageName: "heart.fill"),
+                                Person(name: "Jamie", imageName: "heart"),
+                                Person(name: "Dakota", imageName: "heart.fill")]
+        
+        List(people) {person in
             HStack {
-                Image(systemName: "heart.fill")
-                    .foregroundColor(.red)
-                Text("Sasha")
-            }
-            HStack {
-                Image(systemName: "heart")
-                    .foregroundColor(.black)
-                Text("Jamie")
-            }
-            HStack {
-                Image(systemName: "heart.fill")
-                    .foregroundColor(.red)
-                Text("Dakota")
+                Image(systemName: person.imageName)
+                Text(person.name)
             }
         }
     }
