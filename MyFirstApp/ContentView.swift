2367bc8ea6d7682881d3c8e93931de0a3f086518
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-12 10:43:08 +0900

    Button の理解

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello")
+//        Button{
+//            print("Hited2")
+//        } label: {
+//            Text("Hit me")
+//                .padding()
+//                .frame(width: 150)
+//                .background(.yellow)
+//                .cornerRadius(13)
+//        }
+        
+        Button("Delete" ,role: .destructive) {
+            print("Deleted")
+        }
     }
 }
 
