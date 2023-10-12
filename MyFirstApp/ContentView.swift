1e1ad954dbd94b122f2074ae29b2d9aedb6c77fc
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-12 16:50:37 +0900

    keypad chanllenge1

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -9,30 +9,95 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var isClicked: Bool = false
-    
     var body: some View {
-        ZStack {
-            Color.yellow
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            VStack {
-                Image(isClicked ? "1" : "2")
-                     .resizable()
-                     .aspectRatio(contentMode: .fit)
-                     .frame(width: 400)
-                
-                HStack {
-                    Text("If you want another image,")
-                    Button {
-                        isClicked.toggle()
-                    } label: {
-                        Text("Click")
-                            .font(.system(size:20, weight: .bold, design: .rounded))
-                            .foregroundColor(.black)
-                    }
+        VStack{
+            HStack{
+                Text("1")
+                    .padding(35)
+                    .background(.gray)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                VStack{
+                    Text("2")
+                    Text("ABC")
                 }
+                    .padding(25)
+                    .background(.gray)
+                    .clipShape(Circle())
+                VStack{
+                    Text("3")
+                    Text("DEF")
+                }
+                    .padding(25)
+                    .background(.gray)
+                    .clipShape(Circle())
             }
+            HStack{
+                VStack{
+                    Text("4")
+                    Text("GHI")
+                }
+                    .padding(25)
+                    .background(.gray)
+                    .clipShape(Circle())
+                VStack{
+                    Text("5")
+                    Text("JKL")
+                }
+                    .padding(25)
+                    .background(.gray)
+                    .clipShape(Circle())
+                VStack{
+                    Text("6")
+                    Text("MNO")
+                }
+                    .padding(20)
+                    .background(.gray)
+                    .clipShape(Circle())
+            }
+            HStack{
+                VStack{
+                    Text("7")
+                    Text("PQRS")
+                }
+                    .padding(20)
+                    .background(.gray)
+                    .clipShape(Circle())
+                VStack{
+                    Text("8")
+                    Text("TUV")
+                }
+                    .padding(25)
+                    .background(.gray)
+                    .clipShape(Circle())
+                VStack{
+                    Text("9")
+                    Text("XYZ")
+                }
+                    .padding(25)
+                    .background(.gray)
+                    .clipShape(Circle())
+            }
+            HStack{
+                Text("*")
+                    .padding(35)
+                    .background(.gray)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                VStack{
+                    Text("0")
+                    Text("+")
+                }
+                    .padding(35)
+                    .background(.gray)
+                    .clipShape(Circle())
+                Text("#")
+                    .padding(35)
+                    .background(.gray)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            }
+            Image(systemName: "phone.fill")
+                .padding(35)
+                .background(.green)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
         }
     }
 }
