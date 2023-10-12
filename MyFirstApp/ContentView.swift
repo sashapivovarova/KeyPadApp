e27e799c64d60387fdc4a6f7e0eae28cc6babefe
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-12 16:03:39 +0900

    swiftUI level1 完了

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isClicked: Bool = false
+    
     var body: some View {
-        VStack {
-            Image("1")
-                 .resizable()
-                 .aspectRatio(contentMode: .fit)
-                 .frame(width: 400, height: 200, alignment: .trailing)
-                 .background(.pink)
+        ZStack {
+            Color.yellow
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                Image(isClicked ? "1" : "2")
+                     .resizable()
+                     .aspectRatio(contentMode: .fit)
+                     .frame(width: 400)
+                
+                HStack {
+                    Text("If you want another image,")
+                    Button {
+                        isClicked.toggle()
+                    } label: {
+                        Text("Click")
+                            .font(.system(size:20, weight: .bold, design: .rounded))
+                            .foregroundColor(.black)
+                    }
+                }
+            }
         }
     }
 }
