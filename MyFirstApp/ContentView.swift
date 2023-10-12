132d6411dd64d38cb5b997d4f75d53567b2315aa
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-12 15:45:04 +0900

    padding の理解

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       Image("1")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 200, height: 200)
-            .background(.pink)
+        VStack {
+            Image("1")
+                 .resizable()
+                 .aspectRatio(contentMode: .fit)
+                 .frame(width: 200, height: 200)
+                 .background(.pink)
+                 .padding(.bottom, 100)
+            Image("1")
+                 .resizable()
+                 .aspectRatio(contentMode: .fit)
+                 .frame(width: 200, height: 200)
+                 .padding([.leading, .trailing], 100)
+                 .background(.pink)
+        }
     }
 }
 
